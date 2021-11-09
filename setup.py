4d0a289974f07a33b930f9de7e55cd5695ca024e
dameyerdave <dameyerdave@gmail.com>
2021-11-09 09:14:07 +0100

    increased version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='bioinfo_toolset',
-    version=1.0,
+    version=1.1,
     author='David Meyer',
     author_email='dameyerdave@gmail.com',
     description='Python Rules Evaluator',
