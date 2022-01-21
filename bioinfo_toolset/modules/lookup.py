498ef3b051cb5fa23b096678da2571db4c812844
dameyerdave <dameyerdave@gmail.com>
2022-01-21 16:53:37 +0100

    complement_allele_lookup is now a function that handles multiple alleles in a string

--- bioinfo_toolset/modules/lookup.py
+++ bioinfo_toolset/modules/lookup.py
@@ -1,6 +1,13 @@
-complement_allele_lookup = {
+_complement_allele_lookup = {
     'A': 'T',
     'C': 'G',
     'G': 'C',
     'T': 'A'
 }
+
+
+def complement_allele_lookup(reference_allele):
+    complement_allele = ''
+    for a in reference_allele:
+        complement_allele += _complement_allele_lookup[a]
+    return complement_allele
