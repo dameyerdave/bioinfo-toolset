d2b0471697b70a2bc4a913e2ebef156e2467c5fb
dameyerdave <dameyerdave@gmail.com>
2022-02-10 15:40:11 +0100

    support ins variants correctly

--- bioinfo_toolset/tests/test_hgvs.py
+++ bioinfo_toolset/tests/test_hgvs.py
@@ -10,7 +10,9 @@ class TestHgvs(TestCase):
     def test_from_transcript_change(self):
         hgvs_map_grch37 = {
             '5:149439278:2094_2117>GC': '5:149439278-149439301/GC',
-            'X:53239699:1642_1643GT>AA': 'X:53239699-53239700/TT'
+            'X:53239699:1642_1643GT>AA': 'X:53239699-53239700/TT',
+            '8:37555623:1204_1205insCGACGCACCTCGGCGGCTCCAGCTGCTCCACCTGCAGCGCGC': '8:37555624-37555623/CGACGCACCTCGGCGGCTCCAGCTGCTCCACCTGCAGCGCGC',
+            'X:129190010:5035_5036insT': 'X:129190011-129190010/T'
         }
 
         for test_transcript, expected_region in hgvs_map_grch37.items():
