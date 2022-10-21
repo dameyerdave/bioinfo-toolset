56be6f31ac567121a9eaf3dd1c6101a6b1124507
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 21:43:35 +0000

    Adding tarfile member sanitization to extractall()

--- bioinfo_toolset/modules/vep_offline.py
+++ bioinfo_toolset/modules/vep_offline.py
@@ -90,7 +90,29 @@ class OfflineVep():
             if splitext(file)[1] == '.tar':
                 log.info(f"Untar {file} to {dir}...")
                 with tarfile.open(file) as tar:
-                    tar.extractall(dir)
+                    
+                    import os
+                    
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, dir)
         except Exception as ex:
             raise Exception(f"Cannot unzip file {file}: {ex}")
 
